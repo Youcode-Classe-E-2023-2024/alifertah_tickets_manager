74a2a4b785fe67dc383809405758b3fb0a0e5348
alifertah <alifertah42@gmail.com>
2023-12-13 13:48:09 +0100

    feat : login and signin forms setups ready

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>login</title>
 </head>
 <body>
 <body class="flex justify-center flex-col items-center">
--- src/authentification/register.php
+++ src/authentification/register.php
@@ -1,1 +1,63 @@
-<?php ?>
+<?php
+    if(isset($_POST["register"])){
+        echo("hey");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>register</title>
+</head>
+<body class="flex justify-center">
+
+<div class="w-full max-w-xs">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action="register.php" method="post" name="register-form">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="user" type="text" name="user" placeholder="Username">
+    </div>
+
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+        Email
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" name="email" placeholder="email">
+    </div>
+
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password" placeholder="******************">
+    </div>
+
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="type">
+        type
+      </label>
+      <select class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="select" name="select" >
+        <option value="utilisateur" selected>Utilisateur</option> 
+        <option value="annonceur" >Annonceur  </option> 
+      </select>
+    </div>
+
+    <div class="flex items-center justify-between">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" name="register" value="register">
+        Sign In
+      </button>
+    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="../../index.php">
+      login
+    </a>
+    </div>
+    
+  </form>
+</div>
+
+</body>
+</html>
