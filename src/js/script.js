fb53ad88b566bd5dc63c021166f80f8cc6e81423
alifertah <alifertah42@gmail.com>
2023-12-20 19:26:23 +0100

    feat : hide tages after selecting one

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,21 @@
 let assigneeSelect = document.getElementById("assignees");
+let tags = document.getElementById("tags");
+
+let selectedTags = [];
 let selectedAssignees = [];
+
 assigneeSelect.addEventListener("change", function () {
     let selectedOption = this.options[this.selectedIndex];
     selectedAssignees.push(selectedOption.value);
     selectedOption.style.display = "none";
 });
+
+
+tags.addEventListener("change", function(){
+  let selectedTag = this.options[this.selectedIndex];
+  selectedTags.push(selectedTag.value);
+  selectedTag.style.display = "none";
+})
 
 function submitForm() {
  fetch("process.php", {
@@ -23,3 +34,21 @@ function submitForm() {
       console.error('Error:', error);
   });
 }
+
+function submitTags() {
+  fetch("processTags.php", {
+   method: "POST",
+   headers: {
+     "Content-Type" : "application/json",
+   },
+   body: JSON.stringify({items: selectedAssignees})
+  })
+ 
+  .then(response => response.json())
+  .then(data =>{
+   console.log("Success", data);
+  })
+  .catch((error) => {
+       console.error('Error:', error);
+   });
+ }
